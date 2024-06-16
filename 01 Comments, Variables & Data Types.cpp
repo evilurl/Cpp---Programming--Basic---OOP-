8c73655c65dc914e7abb97dbb6c7f935a613b31f
Muhammad Hassan Raza <141415306+evilurl@users.noreply.github.com>
2024-06-16 19:33:48 +0500

    Comments, Variables & Data Types

---
+++ 01 Comments, Variables & Data Types.cpp
@@ -0,0 +1,79 @@
+//###### COMMENTS ######
+// Signle Line Comment
+// hi welcome to this channel
+
+/* Multiline Comment*/
+/*hi welcome to this channel
+a
+b
+v
+d
+s
+a
+a
+*/
+/**/
+// abc
+
+#include<iostream>
+#include<stdlib.h>
+#include<string>
+using namespace std;
+
+int a = 40;
+
+int sumOfValues(){
+	int a = 30;
+	return a;
+}
+
+int main(){
+
+//###### DATA TYPES ######
+//int a = 6;
+//cout<<a<<endl;
+//
+//float b = 10.5;
+//cout<<b<<endl;
+//
+//char c = 'D';
+//cout<<c<<endl;
+//
+//bool h = true;
+//cout<<h<<endl;
+//
+//double f = 1055555.5;
+//cout<<f<<endl;
+//
+//string x = "Hello";
+//cout<<x<<endl;
+
+//###### VARIABLES ######
+//int a = 20;
+//int b = 30;
+//int sumOfValues = a + b;
+//cout<<sumOfValues;
+
+cout<<sumOfValues();
+
+cout<<a;
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
