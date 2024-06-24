ced962c6604808700cb869c3f7bfbfeab1372011
Muhammad Hassan Raza <141415306+evilurl@users.noreply.github.com>
2024-06-24 13:42:35 +0500

    Control Strcutures( if, else & switch)

---
+++ 03 Conrtrol Structures (if,else and switch).cpp
@@ -0,0 +1,107 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main(){
+
+//	if ///////////////////////////////////////////
+//int age;
+//cout<<"Enter age: ";
+//cin>>age;
+//if(age >= 18){
+//	cout<<"You're an adult";
+//}
+//else{
+//	cout<<"You're a minor";
+//}
+
+// if/else if ///////////////////////////////////////////
+//if(age < 18){
+//	cout<<"You're a minor";
+//}
+//
+//else if( age >=18 && age < 65){
+//	cout<<"You're an adult";
+//}
+//
+//else{
+//	cout<<"You're a senior citizen";
+//}
+
+
+// nested if/else //////////////////////////////////////////
+//int marks;
+//cout<<"Enter marks: ";
+//cin>>marks;
+
+// A = 60, B = 50 , C = 40 , D = 30 , F < 30
+//if(marks >= 60) {
+//cout <<"Grade A";
+//}else{
+//	if(marks >= 50 ){
+//		cout<<"Grade B";
+//	} else{
+//		if(marks >=40){
+//			cout<<"Grade C";
+//		}else{
+//			if(marks >= 30){
+//				cout<<"Grade D";
+//			}else{
+//				cout<<"Grade F";
+//			}
+//		}
+//	}
+//}
+
+// Switch ///////////////////////////////////////////
+/*
+1 = Monday
+2 = Tuesday
+3 = Wednesday
+4 = Thursday
+5 = Friday
+6 = Saturday
+7 = Sunday
+*/
+
+//int day;
+//cout<<"Enter day: ";
+//cin>>day; // 5
+//
+//switch(day){
+//	case 1:
+//		cout<<"Monday\n";
+//		break;
+//		
+//	case 2:
+//		cout<<"Tuesday\n";
+//		break;
+//	
+//	case 3:   
+//		cout <<"Wednesday"<<endl;
+//		break;
+//    
+//	case 4:
+//        cout <<"Thursday"<<endl;
+//        break;
+//        
+//	case 5:
+//        cout <<"Friday"<<endl;
+//        break;
+//        
+//	case 6:
+//        cout <<"Saturday"<<endl;
+//        break;
+//        
+//	case 7:
+//        cout <<"Sunday"<<endl;
+//        break;
+//        
+//	default:
+//        cout <<"Invalid day"<<endl;
+//    
+//}
+
+
+	return 0;
+}
