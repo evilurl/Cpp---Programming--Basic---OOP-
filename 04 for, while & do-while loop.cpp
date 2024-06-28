2c3b593df2076168986c3379bcb53b12004a8392
Muhammad Hassan Raza <141415306+evilurl@users.noreply.github.com>
2024-06-28 19:44:20 +0500

    for, while & do-while loops

---
+++ 04 for, while & do-while loop.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+
+int main(){
+
+//for(int i = 0 ; i<6 ; i++){
+//	cout<<i<<endl;
+//}
+
+//int i = 0;
+//while(i <= 6){
+//	cout<<i<<endl;
+//	
+//	i++;
+//}
+
+//int p = 0;
+//do{
+//	cout<<"hello"<<p<<endl;
+//	
+//	p++;
+//}while(p <= 6);
+
+	return 0;
+}
