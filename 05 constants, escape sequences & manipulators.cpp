00125ebe2a59e352283fc441f8cdc00d2ec6ed43
Muhammad Hassan Raza <141415306+evilurl@users.noreply.github.com>
2024-07-02 17:53:55 +0500

    constants, escape sequences, manipulators

---
+++ 05 constants, escape sequences & manipulators.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+#include<stdlib.h>
+#include<iomanip>
+using namespace std;
+
+
+int main(){
+
+//const int a = 10;
+//cout<<"Value of a is: "<<a;
+//
+//a = 20;
+//cout<<"Value of a is: "<<a;
+
+// Area of circle
+//const float pi = 3.14;
+//int r = 2;
+//
+//float Area;
+//Area = pi * r*r;
+//cout<<"Area of circle is: "<<Area;
+
+// Escape Sequences
+/*
+	\a - alarm
+	\b - backspace
+	\f - form feed
+	\n - new line
+	\r - carriage return
+	\t - tab
+	\' - single quote
+	\" - double quote
+	\\ - display backslash
+*/
+
+//cout<<"1 Hello \aWorld"<<endl;
+//cout<<"2 Hello\bWorld"<<endl;
+//cout<<"3 Hello \fWorld"<<endl;
+//cout<<"4 Hello \nWorld"<<endl;
+//cout<<"5 Hello\rWorld"<<endl;
+//cout<<"6 Hello \tWorld"<<endl;
+//cout<<"7 Hello \'World\'"<<endl;
+//cout<<"8 Hello \"World\""<<endl;
+//cout<<"9 Hello \\World"<<endl;
+
+// C++ Manipulators
+// endl, setw(), setprecission()
+
+//cout<<"My name is: "<<endl;
+//cout<<"My Class is: "<<endl;
+//cout<<"My school is: "<<endl;
+
+// setw()
+//int a, b,c;
+//a = 10;
+//b = 999;
+//c = 2000;
+//
+//cout<<"Value of a: "<<a<<endl;
+//cout<<"Value of b: "<<b<<endl;
+//cout<<"Value of c: "<<c<<endl;
+//
+//cout<<"Value of a: "<<setw(4)<<a<<endl;
+//cout<<"Value of b: "<<setw(4)<<b<<endl;
+//cout<<"Value of c: "<<setw(4)<<c<<endl;
+
+//int n = 3926;
+//double d = 91.5;
+//char str[] = "OOP using C++";
+//
+//cout<<setw(5)<<n<<endl;
+//cout<<setw(8)<<d<<endl;
+//cout<<setw(16)<<str<<endl;
+
+//setprecission()
+
+//double d, n1, n2;
+//n1 = 132.364;
+//n2 = 26.91;
+//
+//d = n1 / n2;
+//
+//cout<<d<<endl;
+//cout<<setprecision(5)<<d<<endl;
+//cout<<setprecision(4)<<d<<endl;
+//cout<<setprecision(3)<<d<<endl;
+//cout<<setprecision(2)<<d<<endl;
+//cout<<setprecision(1)<<d<<endl;
+
+	return 0;
+}
