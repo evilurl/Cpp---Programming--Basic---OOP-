3ca2df64347722c045ec0084cfc2077e78db2073
Muhammad Hassan Raza <141415306+evilurl@users.noreply.github.com>
2024-07-30 18:46:21 +0500

    -

---
+++ 06 Break & Continue Statements.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main() {
+
+//for(int i = 1 ; i<=50 ; i++){
+//	if(i==40){
+//		continue;
+//	}
+//	cout<<i<<endl;
+//}
+
+//for(int day=1 ; day<=30 ; day++){
+//	if(day%3 == 0){
+//		continue;
+//	}
+//
+//	cout<<day<<endl;
+//}
+
+	/* Prime Number Checker */
+//	int n;
+//	cout<<"enter number: ";
+//	cin>>n;
+//
+//	int i;
+//	for(i=2 ; i<n ; i++) {
+//
+//		if(n%i == 0) {
+//			cout<<"Non-prime"<<endl;
+//			break;
+//		}
+//
+//	}
+//	if(i==n) {
+//		cout<<"Prime Number"<<endl;
+//	}
+
+//	int num1, num2;
+//	cout<<"start: ";
+//	cin>>num1;
+//
+//	cout<<"end: ";
+//	cin>>num2;
+//
+//	for(int a=num1 ; a<num2 ; a++) {
+//		int i;
+//		for(i=2 ; i<a ; i++) {
+//			if(a%i == 0) {
+//				break;
+//			}
+//		}
+//		if(i==a) {
+//			cout<<a<<": Prime Number"<<endl;
+//		}
+//
+//	}
+
+	return 0;
+}
